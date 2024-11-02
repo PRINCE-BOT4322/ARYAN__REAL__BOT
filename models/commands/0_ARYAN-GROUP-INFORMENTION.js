0697366a075ced3f7232e653251e889665e07e14
PRINCE-BOT4322 <ik1673254@gmail.com>
2024-11-02 07:20:14 +0530

    Update and rename 0_ARIF-GROUP-INFORMENTION.js to 0_ARYAN-GROUP-INFORMENTION.js

--- models/commands/0_ARYAN-GROUP-INFORMENTION.js
+++ models/commands/0_ARYAN-GROUP-INFORMENTION.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const request = require("request");
 module.exports.config = {
-	name: "groupinf",
+	name: "groupinfo",
 	version: "1.0.0", 
 	hasPermssion: 1,
-	credits: "ARIF BABU",
-	description: "THIS BOT WAS MADE BY MR ARIF BABU",
+	credits: "ARYAN KING",
+	description: "THIS BOT WAS MADE BY MR ARYAN KING",
 	commandCategory: "GROUP INFORMETION", 
 	usages: "PREFIX", 
 	cooldowns: 0,
@@ -40,7 +40,9 @@ module.exports.run = async function({ api, event, args }) {
 			var callback = () =>
 				api.sendMessage(
 					{
-						body: `🌺 GROUP NAME 𒁍 ${threadName}\n🌺 GROUP UID 𒁍 ${id}\n🌺 GROUP APPROVAL 𒁍 ${pd}\n🌺 GROUP EMOJI 𒁍 ${icon}\n🌺 TOTAL MEMBER 𒁍 ${threadMem}\n🌺 TOTAL MALE 𒁍 ${nam}\n🌺 TOTAL FEMALE 𒁍 ${nu}\n🌺TOTAL GROUP ADMIN 𒁍 ${qtv}\n🌺 TOTAL GROUP MESSAGES 𒁍 ${sl}\n\n════════════════════════ ❁\nBOT CREATER BY MR ARIF BABU 🙂✌️`,
+						body: `🌺 𝐆𝐑𝐎𝐔𝐏 𝐈𝐍𝐅𝐎 𒁍 ${threadName}\n🌺 𝐆𝐑𝐎𝐔𝐏 𝐔𝐈𝐃 𒁍 ${id}\n🌺 𝐆𝐑𝐎𝐔𝐏 𝐀𝐏𝐏𝐑𝐎𝐕𝐀𝐋 𒁍 ${pd}\n🌺 𝐆𝐑𝐎𝐔𝐏 𝐄𝐌𝐎𝐉𝐈 𒁍 ${icon}\n🌺 𝐓𝐎𝐓𝐀𝐋 𝐍𝐔𝐌𝐁𝐄𝐑 𒁍 ${threadMem}\n🌺 𝐓𝐎𝐓𝐀𝐋 𝐌𝐀𝐋𝐄 𒁍 ${nam}\n🌺 𝐓𝐎𝐓𝐀𝐋 𝐅𝐄𝐌𝐀𝐋𝐄 𒁍 ${nu}\n🌺 𝐓𝐎𝐓𝐀𝐋 𝐆𝐑𝐎𝐔𝐏 𝐀𝐃𝐌𝐈𝐍 𒁍 ${qtv}\n🌺 𝐓𝐎𝐓𝐀𝐋 𝐆𝐑𝐎𝐔𝐏 𝐌𝐄𝐒𝐒À𝐆𝐄S𒁍 ${sl}\n\n════════════════════════ ❁\n╔╬⓼★⓼╃───────➣™
+🄼🅁 🄱🄾🅃 🄾🅆🄽🄴🅁 🄰🅁🅈🄰🄽𝄠━─━Ⓔ⧐
+╰✾✾®️╀✿✿╀─━ↈⓇ⧐ 🙂✌️`,
 						attachment: fs.createReadStream(__dirname + '/cache/1.png')
 					},
 					event.threadID,
